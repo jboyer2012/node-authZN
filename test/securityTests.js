89de4fa38331d5fc6615616cdd421831f910d681
Justin Boyer <boyer942@aim.com>
2017-06-02 10:42:07 -0400

    Add authentication tests for all endpoints

--- test/securityTests.js
+++ test/securityTests.js
@@ -13,5 +13,27 @@ describe('Authentication', function(){
             // no authentication complete, so request should fail
             .expect(401, done);
     });
+
+    it('should not allow unauthenticated users to deposit funds', function(done){
+        var postData = { "number": "434545", "amount": 400.00 };
+        request(appUnderTest)
+            .post('/banker/deposit')
+            .send(postData)
+            .expect(401, done);
+    });
+
+    it('should not allow unauthenticated users to request a balance', function(done){
+        request(appUnderTest)
+            .get('/banker/account/123456')
+            .expect(401, done);
+    });
+
+    it('should not allow unauthenticated users to transfer funds', function(done){
+        var postData = { "fromNumber": "434545", "toNumber": "123456", "amount": 500 };
+        request(appUnderTest)
+            .post('/banker/transfer')
+            .send(postData)
+            .expect(401, done);
+    });
 });
 
